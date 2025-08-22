e0353846c2a837c7d29de86869f9bfa07111e00d
Hoang-Long <plong12102001@gmail.com>
2025-08-22 17:33:47 +0700

    feat:confirm password

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,4 +1,5 @@
-import type { RegisterOptions } from 'react-hook-form'
+
+import type { RegisterOptions, UseFormGetValues } from 'react-hook-form'
 import type { IFormState } from '~/types/common'
 
 type FORM_VALIDATE_TYPE = {
@@ -52,3 +53,55 @@ export const FORM_VALIDATE: FORM_VALIDATE_TYPE = {
     }
   }
 }
+export const getRules = (getValues?: UseFormGetValues<IFormState>): FORM_VALIDATE_TYPE => ({
+  email: {
+    required: {
+      value: true,
+      message: 'Email là bắt buộc'
+    },
+    pattern: {
+      value: /^\S+@\S+\.\S+$/,
+      message: 'Email không đúng định dạng'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 5 - 160 ký tự'
+    },
+    minLength: {
+      value: 5,
+      message: 'Độ dài từ 5 - 160 ký tự'
+    }
+  },
+  password: {
+    required: {
+      value: true,
+      message: 'Password là bắt buộc'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    }
+  },
+  confirm_password: {
+    required: {
+      value: true,
+      message: 'Nhập lại password là bắt buộc'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    validate:
+      typeof getRules === 'function'
+        ? (value) => value === getValues?.('password') || 'Nhập lại password không khớp'
+        : undefined
+  }
+})
