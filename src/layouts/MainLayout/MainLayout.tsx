8d88ea8dcb87a6bd27bcbaa3ba161545368da3c2
Hoang-Long <plong12102001@gmail.com>
2025-08-27 17:48:24 +0700

    feat:Code UI MainLayout Header

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,13 +1,15 @@
-import Footer from "~/components/Footer"
-import Header from "~/components/Header"
+import Footer from '~/components/Footer'
+import Header from '~/components/Header'
 
-interface IProps{
-  children?:React.ReactNode
+interface IProps {
+  children?: React.ReactNode
 }
-export default function MainLayout({children}:IProps) {
+export default function MainLayout({ children }: IProps) {
   return (
-    <div>    <Header/>
-          {children}
-          <Footer /></div>
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
   )
 }
