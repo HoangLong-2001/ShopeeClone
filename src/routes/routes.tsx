670b6ee3113685e80ec25177c6a046fca5303aec
Hoang-Long <plong12102001@gmail.com>
2025-09-03 16:44:08 +0700

    feat:handle protected route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,19 @@
+import { Navigate, Outlet, type RouteObject } from 'react-router'
 import LoginLayout from '~/layouts/LoginLayout/LoginLayout'
 import MainLayout from '~/layouts/MainLayout'
 import RegisterLayout from '~/layouts/RegisterLayout'
 import Login from '~/pages/Login'
 import ProductList from '~/pages/ProductList'
+import Profile from '~/pages/Profile'
 import Register from '~/pages/Register'
-const routes = [
+const isAuthenticated = false
+function ProtectedRoute() {
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+function RejectedRoute() {
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
+const routes: RouteObject[] = [
   {
     path: '/',
     element: (
@@ -14,20 +23,40 @@ const routes = [
     )
   },
   {
-    path: '/login',
-    element: (
-      <LoginLayout>
-        <Login />
-      </LoginLayout>
-    )
+    path: '',
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: '/profile',
+        element: (
+          <MainLayout>
+            <Profile />
+          </MainLayout>
+        )
+      }
+    ]
   },
   {
-    path: '/register',
-    element: (
-      <RegisterLayout>
-        <Register />
-      </RegisterLayout>
-    )
+    path: '',
+    element: <RejectedRoute />,
+    children: [
+      {
+        path: '/login',
+        element: (
+          <LoginLayout>
+            <Login />
+          </LoginLayout>
+        )
+      },
+      {
+        path: '/register',
+        element: (
+          <RegisterLayout>
+            <Register />
+          </RegisterLayout>
+        )
+      }
+    ]
   }
 ]
 export default routes
