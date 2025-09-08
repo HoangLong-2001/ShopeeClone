233e70f414ff4b1ffd72bf7f6a50a41bf918af8a
Hoang-Long <plong12102001@gmail.com>
2025-09-08 16:50:20 +0700

    feat:handle aside filter ui

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,8 +4,10 @@ import type { FieldValues, RegisterOptions, UseFormRegister } from 'react-hook-f
 interface IInputProps<T extends FieldValues> extends InputHTMLAttributes<HTMLInputElement> {
   register?: UseFormRegister<T>
   rules?: RegisterOptions<T>
-  name: any
+  name?: any
   errorMessage?: string
+  classNameInput?: string
+  classNameError?: string
 }
 export default function Input<T extends FieldValues>({
   type,
@@ -14,6 +16,8 @@ export default function Input<T extends FieldValues>({
   className,
   placeholder,
   name,
+  classNameError = 'mt-1 min-h-[1rem] text-sm text-red-600',
+  classNameInput = 'w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm',
   errorMessage,
   rules
 }: IInputProps<T>) {
@@ -23,11 +27,11 @@ export default function Input<T extends FieldValues>({
       <input
         type={type}
         autoComplete={autoComplete}
-        className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+        className={classNameInput}
         placeholder={placeholder}
         {...registerResult}
       />
-      <div className='mt-1 min-h-[1rem] text-sm text-red-600'>{errorMessage}</div>
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   )
 }
