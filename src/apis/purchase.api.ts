6d45ce8cd17c59aec2b44afd1c2f891ec546e83a
Hoang-Long <plong12102001@gmail.com>
2025-09-18 18:07:22 +0700

    feat: handle purchase api

---
+++ src/apis/purchase.api.ts
@@ -0,0 +1,16 @@
+import type { IResponse } from '~/types/common.type'
+import type { Purchase, PurchaseListStatus } from '~/types/purchase.type'
+import http from '~/utils/http'
+
+const URL = '/purchases'
+
+export const addToCart = async (body: { product_id: string; buy_count: number }) => {
+  const response = await http.post<IResponse<Purchase>>(`${URL}/add-to-cart`, body)
+  return response.data
+}
+export const getPurchases = async (params: { status: PurchaseListStatus }) => {
+  const response = await http.get(URL, {
+    params
+  })
+  return response.data
+}
