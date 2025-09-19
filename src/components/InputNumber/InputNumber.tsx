fda2a630efb57467af2780601eb9ff7c354bcebb
Hoang-Long <plong12102001@gmail.com>
2025-09-19 18:25:44 +0700

    feat: handle input local state

--- src/components/InputNumber/InputNumber.tsx
+++ src/components/InputNumber/InputNumber.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, type InputHTMLAttributes } from 'react'
+import { forwardRef, useState, type InputHTMLAttributes } from 'react'
 export interface InputNumberProps extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
   classNameInput?: string
@@ -12,19 +12,22 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
     classNameError = 'mt-1 min-h-[1rem] text-sm text-red-600',
     classNameInput = 'w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm',
     errorMessage,
+    value = '',
     ...rest
   },
   ref
 ) {
+  const [localStateValue, setLocalStateValue] = useState<string>(value as string)
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
-    if ((/^\d+$/.test(value) || value === '') && onChange) {
-      onChange(e)
+    if (/^\d+$/.test(value) || value === '') {
+      onChange?.(e)
+      setLocalStateValue(value)
     }
   }
   return (
     <div className={className}>
-      <input {...rest} className={classNameInput} onChange={handleChange} ref={ref} />
+      <input {...rest} className={classNameInput} onChange={handleChange} value={value || localStateValue} ref={ref} />
       <div className={classNameError}>{errorMessage}</div>
     </div>
   )
