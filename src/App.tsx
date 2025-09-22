fc42e1b024ca98842e24c05139d9c38fd7c3fc15
Hoang-Long <plong12102001@gmail.com>
2025-09-22 16:11:28 +0700

    feat: handleScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useLocation } from 'react-router'
 import useRoutesElement from './routes'
 import { ToastContainer } from 'react-toastify'
+import { useEffect } from 'react'
 function App() {
   const element = useRoutesElement()
+  const { pathname, search } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // hoặc "auto"
+    })
+  }, [pathname, search])
+  
   return (
     <div>
       {element} <ToastContainer />
