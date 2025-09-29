121387aa4925bcddca03ec4d125a7937612be3b5
Hoang-Long <plong12102001@gmail.com>
2025-09-29 17:40:40 +0700

    feat: handle user sideNav ui

--- src/pages/User/layouts/UserLayout/UserLayout.tsx
+++ src/pages/User/layouts/UserLayout/UserLayout.tsx
@@ -3,9 +3,15 @@ import UserSideNav from '../../components/UserSideNav'
 
 export default function UserLayout() {
   return (
-    <div>
-      <UserSideNav />
-      <Outlet />
+    <div className='border-b-4 border-b-orange bg-neutral-100 py-6'>
+      <div className='container grid grid-cols-1 md:grid-cols-12'>
+        <div className='md:col-span-3 lg:col-span-2'>
+          <UserSideNav />
+        </div>
+        <div className='md:col-span-9 lg:col-span-10'>
+          <Outlet />
+        </div>
+      </div>
     </div>
   )
 }
